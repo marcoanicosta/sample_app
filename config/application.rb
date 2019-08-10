2e56844e39fb654fa19e479f0f0089286aaa737b
marcohayah <your.marco.nafafe@hayahreach.com>
2019-08-10 21:21:33 +0000

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -1,10 +1,12 @@
 require_relative 'boot'
-
+require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
+
 
 module SampleApp
   class Application < Rails::Application
@@ -14,5 +16,8 @@ module SampleApp
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    
+    # Include the authenticity token in remote forms.
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -17,8 +17,14 @@ Rails.application.routes.default_url_options[:host] = "https://e1688578b0924065a
   get  '/login',   to: 'sessions#new'
   post  '/login',   to: 'sessions#create'
   delete  '/logout',   to: 'sessions#destroy'
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
     resources :users
     resources :account_activations, only: [:edit]
     resources :password_resets,     only: [:new, :create, :edit, :update]
     resources :microposts,          only: [:create, :destroy]
+    resources :relationships,       only: [:create, :destroy]
 end
